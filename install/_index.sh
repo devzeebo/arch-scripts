3edec5c8145034f3aafb245fc2ef4259a791eaa7
Eric Siebeneich <eric.siebeneich@gmail.com>
2020-02-28 15:09:41 -0600

    Added bootstrap installer

---
+++ install/_index.sh
@@ -0,0 +1,12 @@
+export ARCH_INSTALL_SCRIPTS_VERSION=0.0.2
+
+get_script() {
+    curl "https://cdn.jsdelivr.net/gh/devzeebo/arch-scripts@${ARCH_INSTALL_SCRIPTS_VERSION}/install/$1" --output $1
+    chmod +x $1
+}
+
+get_script format.sh
+get_script install-arch.sh
+
+./format.sh
+./install-arch.sh
