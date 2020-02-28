aaee33005731da67e3cc4021c7a94b8052fe90f6
Eric Siebeneich <eric.siebeneich@gmail.com>
2020-02-28 14:22:00 -0600

    Testing out menu

---
+++ format.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+prompt_part() {
+    local __resultVar=$1
+
+    echo '\n\n'
+    echo 'Available partitions:'
+    echo
+
+    fdisk -l | grep '^/dev'
+
+    echo "Select $2 partition:"
+
+    mapfile -t local options < <( fdisk -l | sed -n 's|^\([/a-z0-9]\+\).*|\1|p' )
+    select local opt in "${options[@]}"; do
+        case $opt in
+            /dev*)
+                eval $__resultVar="'$opt'"
+                break
+                ;;
+            *)
+                echo 'Invalid Option'
+                ;;
+        esac
+    done
+}
+
+echo
+
+prompt_part ext4_part 'ext4'
+
+echo "Formatting ext4 partition: $ext4_part"
