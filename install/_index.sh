67125f1dc15630da1704b106456925ecf86c9112
Eric Siebeneich <eric.siebeneich@gmail.com>
2020-02-28 16:00:42 -0600

    Run script options

--- install/_index.sh
+++ install/_index.sh
@@ -1,17 +1,29 @@
-export ARCH_INSTALL_SCRIPTS_VERSION=0.0.2
+export ARCH_INSTALL_SCRIPTS_VERSION=0.0.4
 
 get_script() {
     curl "https://cdn.jsdelivr.net/gh/devzeebo/arch-scripts@${ARCH_INSTALL_SCRIPTS_VERSION}/install/$1" --output $1
     chmod +x $1
 }
 
+run_script() {
+    echo "Run $2? [yN]"
+
+    local response
+
+    read $response
+
+    if [[ $response == "y" ]]; then
+        $($1)
+    fi
+}
+
 get_script format.sh
 get_script install-arch.sh
 get_script pick-timezone.sh
 
-./format.sh
-./install-arch.sh
+run_script ./format.sh 'Format'
+run_script ./install-arch.sh 'Install Arch'
 
 arch-chroot /mnt
 
-./pick-timezone.sh
+run_script ./pick-timezone.sh 'Select Timezone'
