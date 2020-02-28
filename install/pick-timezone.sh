82a4812221bf6423543ff3d618afa5f91bb86255
Eric Siebeneich <eric.siebeneich@gmail.com>
2020-02-28 17:37:02 -0600

    Typos in post install

--- install/pick-timezone.sh
+++ install/pick-timezone.sh
@@ -3,7 +3,7 @@
 read -p 'Enter timezone: (US/Central) ' timezone
 
 if [[ -z $timezone ]]; then
-    $timezone = 'US/Central'
+    $timezone='US/Central'
 fi
 
 ln -sf "/usr/share/zoneinfo/$1" /etc/localtime
