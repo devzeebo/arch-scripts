a7ad3576a66c689292cd734e53a0c9a1b4287033
Eric Siebeneich <eric.siebeneich@gmail.com>
2020-02-28 17:17:17 -0600

    Prompting

--- install/_index.sh
+++ install/_index.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export ARCH_INSTALL_SCRIPTS_VERSION=0.0.16
+export ARCH_INSTALL_SCRIPTS_VERSION=0.0.17
 
 get_script_from() {
     echo "Downloading $2..."
@@ -15,8 +15,7 @@ get_script() {
 run_script() {
 
     local response
-
-    read -p "Run $2? [yN]" $response
+    read -p -n1 "Run $2? [y,n]" response
 
     if [[ "$response" == "y" ]]; then
         echo "Running $2"
