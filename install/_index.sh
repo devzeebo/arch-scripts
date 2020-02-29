d840719cc39b5ec8b1fa17f1cd75329f2b1a012c
Eric Siebeneich <eric.siebeneich@gmail.com>
2020-02-28 18:06:54 -0600

    Typos in post install

--- install/_index.sh
+++ install/_index.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export ARCH_INSTALL_SCRIPTS_VERSION=0.0.25
+export ARCH_INSTALL_SCRIPTS_VERSION=0.0.26
 
 get_script_from() {
     echo "Downloading $2..."
--- install/localization.sh
+++ install/localization.sh
@@ -3,7 +3,7 @@
 read -p 'Enter locale: (en_us.UTF-8)' locale
 
 if [[ -z $locale ]]; then
-    local='en_us.UTF-8'
+    locale='en_us.UTF-8'
 fi
 
 sed -i "s|^#\($locale.*\)|\1|g" /etc/locale.gen
